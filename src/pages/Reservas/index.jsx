4e18694fa1a44692406102a9ccdf6e3180d17606
ricardo <ricardo.carvalho@iteris.com.br>
2020-03-05 15:30:59 -0300

    Adicionando a reserva com redux

--- src/pages/Reservas/index.jsx
+++ src/pages/Reservas/index.jsx
@@ -3,22 +3,25 @@ import React from "react";
 import { MdDelete } from "react-icons/md";
 
 import "./style.css";
+import { useSelector } from "react-redux";
 
 export default function Reservas() {
+  const reservas = useSelector(state => state.reserva);
+
   return (
     <>
-      <h1 className="title">Você solicitou 1 reserva</h1>
-      <div className="reservas">
-        <img
-          src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg"
-          alt=""
-        ></img>
-        <strong>Viagem para Maceio</strong>
-        <span>Quantidade: 2</span>
-        <button type="button" onClick={() => {}}>
-          <MdDelete size={20} color="#443737" />
-        </button>
-      </div>
+      <h1 className="title">Você solicitou {reservas.length} reserva</h1>
+
+      {reservas.map(res => (
+        <div className="reservas" key={res.id}>
+          <img src={res.image} alt={res.title}></img>
+          <strong>{res.title}</strong>
+          <span>Quantidade: 2</span>
+          <button type="button" onClick={() => {}}>
+            <MdDelete size={20} color="#443737" />
+          </button>
+        </div>
+      ))}
       <footer>
         <button type="button">Solicitar Reservas</button>
       </footer>
