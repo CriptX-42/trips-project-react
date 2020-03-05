9e391fd234d430a04c89a22a4365aa0dafcbedf0
ricardo <ricardo.carvalho@iteris.com.br>
2020-03-05 13:51:01 -0300

    Consumindo dados de viagem na pagina Home e também colocando estilo nela. WIP: Colocando estilo na pagina de reservas

--- src/Reservas/index.jsx
+++ src/Reservas/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import './style.css';
+
 export default function Reservas() {
 
   return(
     <>
-      <h1>Reservas</h1>
+      <h1>Você solicitou 1 reserva</h1>
+      <div className="reservas">
+        <img src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg" alt=""></img>
+        <strong>Viagem para Maceio</strong>
+      </div>
     </>
   )
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,25 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import api from '../../services/api';
+import React, { useState, useEffect } from "react";
+
+import {MdFlightTakeoff} from 'react-icons/md';
+
+import api from "../../services/api";
+import './style.css';
 
 export default function Home() {
-
-  const [ trips, setTrips ] = useState([]);
+  const [trips, setTrips] = useState([]);
 
   useEffect(() => {
-
     async function loadApi() {
-      const response = await api.get('trips');
+      const response = await api.get("trips");
       setTrips(response.data);
     }
 
     loadApi();
+  }, []);
 
-  }, [])
-
-  return(
+  return (
     <>
-      <h1>Home</h1>
+      <div className="box">
+        {trips.map(trip => (
+          <li key={trip.id}>
+            <img src={trip.image} alt={trip.title} />
+            <strong>{trip.title}</strong>
+            <span>Status: {trip.status ? "Disponível" : "Indisponível"}</span>
+            <button type="button" onClick={() => {}}>
+              <div><MdFlightTakeoff size={16} color="#FFF"/></div>
+              <span>Solicitar Reserva</span>
+            </button>
+          </li>
+        ))}
+      </div>
     </>
-  )
-
+  );
 }
