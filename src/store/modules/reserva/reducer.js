2c126920e032e9abab780812cfa4fd38262bba77
ricardo <ricardo-svc@live.com>
2020-03-08 22:00:15 -0300

    corrigind bug do data

--- src/store/modules/reserva/reducer.js
+++ src/store/modules/reserva/reducer.js
@@ -4,13 +4,8 @@ export default function reserve(state = [], action) {
   switch (action.type) {
     case 'ADD_RESERVE_SUCCESS':
       return produce(state, draft => {
-        const tripIndex = draft.findIndex(trip =>
-          trip.id === action.trip.id);
-        if (tripIndex >= 0) {
-          draft[tripIndex].amount += 1;
-        } else {
-          draft.push({...action.trip, amount: 1});
-        }
+        console.log(action.trip)
+        draft.push(action.trip.data)
       });
     case 'REMOVE_RESERVE':
       return produce(state, draft => {
--- src/store/modules/reserva/saga.js
+++ src/store/modules/reserva/saga.js
@@ -1,11 +1,20 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
-import { addReserveSuccess } from './actions';
+import { select, call, put, all, takeLatest } from 'redux-saga/effects';
+import { updateAmountReserve, addReserveSuccess } from './actions';
 import api from '../../../services/api';
 
 function* addToReserve ({id}) {
-    const response = yield call(api.get, `trips/${id}`);
-
-    yield put(addReserveSuccess(response.data));
+    const tripExist = yield select(state => state.reserva.find(trip => trip.data.id === id))
+    if(tripExist) {
+        const amount = tripExist.amount + 1;
+        yield put (updateAmountReserve(id, amount))
+    }
+    else {
+        const response = yield call(api.get, `trips/${id}`);
+        const data = {
+            ...response, amount: 1
+        }
+        yield put(addReserveSuccess(data))
+    }
 }
 
 export default all([
